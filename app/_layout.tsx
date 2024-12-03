c2c2c662a11f258087df40ceb11019984d3f3377
MuhammadTalha17 <121374986+MuhammadTalha17@users.noreply.github.com>
2024-12-03 22:20:01 +0500

    Profile and Chat Screens

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,11 +7,13 @@ import "../global.css";
 import { ClerkProvider, ClerkLoaded } from "@clerk/clerk-expo";
 import { tokenCache } from "@/lib/auth";
 import { StatusBar } from "react-native";
+import { LogBox } from "react-native";
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
+
+LogBox.ignoreLogs(["Clerk:"]);
 
 export default function RootLayout() {
   const [loaded] = useFonts({
